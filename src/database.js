71a4ec7f113c497bf40345a98b6fa193dad230ba
paulina-11 <paulinavila.lopez@gmail.com>
2022-12-12 23:09:32 -0600

    creacion de los modelos user y movie

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/movie", {
+    await mongoose.connect("mongodb://127.0.0.1:27017/movies_catalog", {
       useNewUrlParser: true,
     });
     console.log("conectado a la base ✅");
---
+++ src/models/Movie.js
@@ -0,0 +1,27 @@
+import {Schema, model} from 'mongoose'
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+});
+
+
+export default model("Movie", movieSchema);
